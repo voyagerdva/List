e5b9c1bafa13e2cedd73f6090b7c2c25c63086a5
voyagerdva <mariner-9@yandex.ru>
2022-11-29 15:39:19 +0300

    modify ListLib

--- src/main/java/ru.zhuravl/List.java
+++ src/main/java/ru.zhuravl/List.java
@@ -1,4 +1,30 @@
 package ru.zhuravl;
 
 public class List {
+
+    byte[] createNewList(byte size) {
+        return new byte[size];
+    }
+
+    public void push(String[] arr, String item) {
+        System.out.println("This is method public void push(String item)");
+    }
+
+    public void delete(int position) {
+        System.out.println("This is method public void delete(int position)");
+    }
+
+    public String take() {
+        return "This is method public String take()";
+    }
+
+    public void printAll() {
+        System.out.println("This is method public void printAll()");
+    }
+
+    public void clean() {
+        System.out.println("This is method public void clean()");
+    }
+
+
 }
