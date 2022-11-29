653fe1f09a9084ea59d03dc2807719231f5ee123
voyagerdva <mariner-9@yandex.ru>
2022-11-29 20:02:41 +0300

    modify InterfaceList & increment version 1.0.2

--- src/main/java/ru.zhuravl/List.java
+++ src/main/java/ru.zhuravl/List.java
@@ -7,16 +7,19 @@ public class List implements InterfaceList{
     }
 
     public void remove(int position) {
-         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     @Override
     public String get(int i) {
+        System.out.println();
         return null;
     }
 
     @Override
     public int size() {
+        System.out.println();
         return 0;
     }
 }
