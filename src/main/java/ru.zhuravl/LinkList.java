5b4ef7d957ac056f25d64f899b3931236a187655
voyagerdva <mariner-9@yandex.ru>
2022-11-30 23:34:11 +0300

    Made straight liks - from previous to next node; increment version; move NODE class into LinkList

--- src/main/java/ru.zhuravl/LinkList.java
+++ src/main/java/ru.zhuravl/LinkList.java
@@ -1,31 +1,80 @@
 package ru.zhuravl;
 
 public class LinkList implements InterfaceList{
-    public Node tail;
-    public Node currentNode;
-    public int position = 0;
 
-//    public void LinkList(String data) {
-//        this.tail = new Node(data, null);
-//        this.tail.position = 0;
-//        this.currentNode = tail;
+    public class Node {
+        String value;
+        Node previous;
+        Node next;
+
+//    Node(String value, Node previous) {
+//        this.value = value;
+//        this.previous = previous;
 //    }
+
+        Node(String value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+
+    Node tail;
+    Node currentNode;
+    Node head;
 
     LinkList() {
         initZero();
     }
 
-    public void initZero() {
+    void initZero() {
         this.tail = null;
-        this.currentNode = tail;
+        this.head = tail;
+        System.out.println("Begin: Create new list with 0 elements:");
+        System.out.println("-----");
     }
 
+
+// !!! это рабочий метод. Работает "в обратную сторону". Попробую сделать чтоб работал прямо.
+//    void add(String data) {
+//        Node node = new Node(data, currentNode);
+//        currentNode = node;
+//        currentNode.position = position++;
+//        System.out.println("    position = " + currentNode.position);
+//    }
+
     public void add(String data) {
-        Node node = new Node(data, currentNode);
-        currentNode = node;
-        currentNode.position = position++;
-        System.out.println("    position = " + currentNode.position);
+        if (tail == null && head == tail) {
+            Node node = new Node(data, null);
+            tail = node;
+            head = node;
+            currentNode = node;
+            System.out.println("Add FIRST NODE - " + node.value);
+            return;
+        }
+        currentNode = head;
+        Node node = new Node(data, null);
+        head = node;
+        System.out.println("ADDED NEXT NODE - " + node.value);
     }
+
+
+
+    public void printList() {
+        Node ref = tail;
+        while (ref != null) {
+            System.out.printf("%s ", ref.value);
+            ref = ref.previous;
+        }
+    }
+
+
+
+
+
+
+
+
 
     public void printAll() {
         Node ref = tail;
